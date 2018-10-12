15271205d45b9ebe709342e4a93db17288e7f9dc
marlous <goonecat@foxmail.com>
2018-10-13 03:36:30 +0800

    add

---
+++ 发布 HEXO 博客与备份博文到 Github 仓库脚本/自动发布博客.bat
@@ -0,0 +1,16 @@
+@echo off
+rem @author：Marlous 2018/10/13
+rem 脚本说明：自动发布博客脚本
+
+rem ---设置---博客所在盘符，目录。
+set disk=E:
+set blogPath=E:\WorkZone\Repositories\my-blog
+
+%disk%
+cd %blogPath%
+
+call  hexo clean
+call  hexo g
+call  hexo d
+
+pause
